f437de34b7bfb7d3677b191c6a44364254bc1439
Mikhail Bukharov <mikhail.bukharov@gmail.com>
2024-12-03 10:21:32 +0300

    model Film is ready

--- movie_project/films/models.py
+++ movie_project/films/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+class Film(models.Model):
+    name = models.CharField(max_length=100, verbose_name="Название фильма")
+    description = models.TextField(verbose_name="Описание")
+    comment = models.CharField(max_length=100, verbose_name="Отзыв о фильме")
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = "Фильм"
+        verbose_name_plural = "Фильмы"
--- movie_project/films/views.py
+++ movie_project/films/views.py
@@ -1,9 +1,11 @@
 from django.shortcuts import render
+from .models import Film
 
 # Create your views here.
 
 def index_films(request):
-    return render(request, 'films/index.html',)
+    films = Film.objects.all()
+    return render(request, 'films/index.html',{'films':films})
 
 def add_film(request):
     return render(request, 'films/new_film.html',)
